3b2771ad482464007798a3fcf0c340ca33e7c196
Abdulkarim Getachew <abdulkarimgmohammed@gmail.com>
2023-04-01 11:33:36 +0300

    Update common_webview.dart

--- lib/common_webview.dart
+++ lib/common_webview.dart
@@ -3,8 +3,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 class CommonWebView extends StatefulWidget {
   final String url;
-  final String title;
-  CommonWebView({required this.url, required this.title});
+  CommonWebView({required this.url});
   @override
   _CommonWebViewState createState() => _CommonWebViewState();
 }
@@ -14,16 +13,13 @@ class _CommonWebViewState extends State<CommonWebView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
         body: WebView(
-          zoomEnabled: true,
-          initialUrl: widget.url,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller = webViewController;
-          },
-        ));
+      zoomEnabled: true,
+      initialUrl: widget.url,
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (WebViewController webViewController) {
+        _controller = webViewController;
+      },
+    ));
   }
 }
